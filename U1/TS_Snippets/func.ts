6809013b32650c85dce7b2640d486dd0351f5005
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-04 09:29:06 +0530

    new commit

---
+++ U1/TS_Snippets/func.ts
@@ -0,0 +1,12 @@
+function addition(a:number ,b: number): number
+{
+    return a + b;
+}
+console.log(addition(5, 10));
+
+
+const mul=(a:number ,b: number): number=>
+{
+    return a * b;
+}
+console.log(mul(5, 4));
