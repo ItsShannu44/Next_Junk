63738d3eed37124fa033e957ac75dd387147322d
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-04 08:56:45 +0530

    Initial commit

---
+++ U1/TS_Snippets/prgrm_4.ts
@@ -0,0 +1,14 @@
+let a:number = 10;
+let b:number =20;
+
+let add= a+b;
+let sub= a-b;
+let mul= a*b;
+let div= a/b;
+let mod= a%b;
+
+console.log("Addition =",add);
+console.log("Subtraction =",sub);
+console.log("Multiplication =",mul);
+console.log("Division =",div);
+console.log("Mod =",mod);
