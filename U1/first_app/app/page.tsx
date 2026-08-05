a9b1fa70cc25b9fb78b40bc038cd6ef3c94ed1cd
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-05 12:00:10 +0530

    init

--- U1/first_app/app/page.tsx
+++ U1/first_app/app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div style={{ textAlign:"center",padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1 style={{fontSize: '100px'}}>Hello User</h1>
-      <p style={{color: 'yellow', textWrap:'wrap', textAlign:'center', width: '1000px'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde neque optio autem atque eveniet dolor, esse dolores perferendis excepturi reiciendis aspernatur voluptas quos in quisquam harum nulla repellat impedit dolore.</p>
+
+// export default function Home() {
+//   return (
+//     <div style={{ textAlign:"center",padding: '2rem', fontFamily: 'sans-serif' }}>
+//       <h1 style={{fontSize: '100px'}}>Hello User</h1>
+//       <p style={{color: 'yellow', textWrap:'wrap', textAlign:'center', width: '1000px'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde neque optio autem atque eveniet dolor, esse dolores perferendis excepturi reiciendis aspernatur voluptas quos in quisquam harum nulla repellat impedit dolore.</p>
+//     </div>
+//   );
+// }
+
+export default function Home()
+{
+  const student={
+    name: "Bob",
+    age: 26,
+    course: "MCA"
+  };
+  
+  return(
+    <div style={{textAlign:'center', marginTop:'20vh', justifyContent:'center', backgroundColor:'grey', width:'max-content', color:'black', padding:'50px'}}>
+      <h2>Student Details</h2>
+      <p>Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+      <p>course: {student.course}</p>
     </div>
   );
 }
