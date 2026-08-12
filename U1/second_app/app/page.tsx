3035766e4f4953ee65478b78827335394874e2ed
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-12 12:26:11 +0530

    new app

---
+++ U1/second_app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <center>
+      <h1>Hello Man!</h1>
+      <p>How may i help you..</p>
+      </center>
+    </div>
+  );
+}
