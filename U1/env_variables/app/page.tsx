b74c0f5eb6e96662165781aa572c5621a683dd61
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-13 14:41:25 +0530

    env_var_exec

--- U1/env_variables/app/page.tsx
+++ U1/env_variables/app/page.tsx
@@ -5,6 +5,8 @@ const apiUrl = process.env.API_URL;
     <div>
       <h1>{appName}</h1>
       <p>API URL: {apiUrl}</p>
+      <h1>{process.env.NEXT_PUBLIC_APP_TITLE}</h1>
+      <p>College: {process.env.COLLEGE_NAME}</p>
     </div>
   );
 }
