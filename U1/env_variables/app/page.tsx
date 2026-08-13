c9896ea684ec1d8c45c3535b22a624ff9e8ba626
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-13 14:21:53 +0530

    env_var_exec

---
+++ U1/env_variables/app/page.tsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+
+export default function Home() {
+const appName= process.env.APP_NAME;
+const apiUrl = process.env.API_URL;
+  return (
+    <div>
+      <h1>{appName}</h1>
+      <p>API URL: {apiUrl}</p>
+    </div>
+  );
+}
