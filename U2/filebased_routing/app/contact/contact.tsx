a8302c26d1c89728c03cdf9ea657b37bc1a0ebcc
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-18 09:51:00 +0530

    file based routing

---
+++ U2/filebased_routing/app/contact/contact.tsx
@@ -0,0 +1,8 @@
+export default function Contact(){
+    return(
+        <div>
+            <h1>This is contact info</h1>
+            <p>India: +91 80097-70099</p>
+        </div>
+    );
+}
--- U2/filebased_routing/app/page.tsx
+++ U2/filebased_routing/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import About from "./about/about";
+import Contact from "./contact/contact";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello Everyone</h1>
       <p>This is home page of file based routing app.</p>
-      <About />
+      <About /><br></br>
+      <Contact/>
     </div>
   );
 }
