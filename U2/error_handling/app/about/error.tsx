69eada707d9c2540847746136487bec92ca1860e
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-08-25 09:58:43 +0530

    error_page

--- U2/error_handling/app/about/error.tsx
+++ U2/error_handling/app/about/error.tsx
@@ -1,27 +1,16 @@
-// "use client";
+"use client";
 
-// export  default function Error({
-//     error, reset,
-// }:{
-//     error: Error& {digest?: string};
-//     reset: ()=>void;
-// }){
-//     return(
-//         <div>
-//             <h2>Something went wrong!</h2>
+export  default function Error({
+    error, reset,
+}:{
+    error: Error& {digest?: string};
+    reset: ()=>void;
+}){
+    return(
+        <div>
+            <h2>Something went wrong!</h2>
 
-//             <button onClick={() => reset()}>Try Again</button>
-//         </div>
-//     )
-// }
-
-async function getStudents(){
-    const response = await fetch(
-        "https://example.com/api/students"
-    );
-    if(!response.ok)
-    {
-        throw new Error("Failed to fetch students")
-    }
-    return response.json();
+            <button onClick={() => reset()}>Try Again</button>
+        </div>
+    )
 }
---
+++ U2/error_handling/app/error.tsx
@@ -0,0 +1,22 @@
+
+async function getStudents(){
+    const response = await fetch(
+        "https://example.com/api/students"
+    );
+    if(!response.ok)
+    {
+        throw new Error("Failed to fetch students")
+    }
+    return response.json();
+}
+ export default  async function StudentsPage() {
+    const students= await getStudents();
+    return(
+        <div>
+            <h1>Students</h1>
+            {students.map((student: any)=>(
+                
+            ))}
+        </div>
+    )
+ }
