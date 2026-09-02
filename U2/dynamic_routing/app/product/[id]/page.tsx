da69d797619fc4cf86842998f5b52164eb58ee0c
itsshannu44 <shanmukhakabbinagadde@gmail.com>
2026-09-02 11:15:46 +0530

    slug_updates

--- U2/dynamic_routing/app/product/[id]/page.tsx
+++ U2/dynamic_routing/app/product/[id]/page.tsx
@@ -1,8 +1,33 @@
-export default async function ProductPage({ 
+type Product = {
+    id: string;
+    name: string;
+};
+
+const products: Product[] = [
+    { id: "101", name: "iPhone 15" },
+    { id: "102", name: "Samsung Galaxy S24" },
+    { id: "103", name: "MacBook Air" },
+    { id: "104", name: "Dell XPS 13" },
+    { id: "abc", name: "Product ABC" },
+];
+
+export default async function ProductPage({
     params,
-}:{
-    params: Promise<{id: string}>
-}){
-    const {id} = await params;
-    return <h1>Product ID: {id}</h1>;
+}: {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+
+    const product = products.find((product) => product.id === id);
+
+    if (!product) {
+        return <h1>Product not found</h1>;
+    }
+
+    return (
+        <div>
+            <h1>Product Name: {product.name}</h1>
+            <p>Product ID: {product.id}</p>
+        </div>
+    );
 }
--- U2/dynamic_routing/app/product/page.tsx
+++ U2/dynamic_routing/app/product/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 
-export default function Products(){
-    return(
+export default function Products() {
+    return (
         <div>
-            <Link href="/products/101">Product 101</Link> <br/>
-            <Link href="/products/102">Product 102</Link> <br/>
-            <Link href="/products/103">Product 103</Link> <br/>
-            <Link href="/products/104">Product 104</Link> <br/>
+            <Link href="/product/101">Product 101</Link> <br />
+            <Link href="/product/102">Product 102</Link> <br />
+            <Link href="/product/103">Product 103</Link> <br />
+            <Link href="/product/104">Product 104</Link> <br />
+            <Link href="/product/abc">Product ABC</Link> <br />
         </div>
-    )
+    );
 }
